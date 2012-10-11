94ee40a1e571f927ee1da8d2dd2f093d4b53c2f6
Rob Whelan <whelan@gmail.com>
2012-10-11 10:23:38 -0400

    commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
   def index
+    if params[:q]
+      puts 'F0000:' + params[:q]
+    end
+    
     @posts = Post.all
     @foo = 'bar'
   end
@@ -10,6 +14,11 @@ class PostsController < ApplicationController
   
   def create
     @post = Post.new(params[:post])
+    respond_to do |format|
+      if @post.save
+        format.html {redirect_to root_path,
+          notice: 'successfully created post' }
+          
   end
 
 end
